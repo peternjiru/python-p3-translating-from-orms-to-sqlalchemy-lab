ea6ee1d2a98c10c9a3f0d0182b5c5ec46dfacc63
Peter Njiru <peternjirum@gmail.com>
2023-08-06 15:04:49 -0400

    completes lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,63 @@
 from models import Dog
+from sqlalchemy import (create_engine,Column,Integer, String) 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from models import Base, Dog
+from testing.conftest import db_dir, SQLITE_URL
 
-def create_table(base):
-    pass
+engine = create_engine(SQLITE_URL)
+Base.metadata.create_all(engine)
+Session = sessionmaker(engine)
+session = Session()
 
+Base = declarative_base()
+
+class Dog(Base):
+    __tablename__ = "dogs" 
+
+    id = Column(Integer(),  primary_key=True)
+    name = Column(String())
+    breed = Column(String()) 
+
+def create_table(Base, engine):   
+    if __name__ == '__main__':
+        # engine = create_engine('sqlite:///dogs.db')
+        # engine = create_engine(SQLITE_URL)
+        Base.metadata.create.all(engine)
+    
 def save(session, dog):
-    pass
-
+    print(dog.name)
+    session.add(dog)
+    session.commit()
+    print(f"new dog {dog.name}")
+    return session.query(Dog).first().name
+    
 def get_all(session):
-    pass
+    all_dogs = []
+    all_dogs = session.query(Dog).all()
+    return all_dogs
 
 def find_by_name(session, name):
-    pass
-
+    return session.query(Dog).filter(Dog.name == name).first()
+ 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+  return session.query(Dog).filter(Dog.name == name , Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+   print(dog.breed, breed)
+   session.query(Dog).update({Dog.breed : breed})
+   session.commit()
+
+
+
+
+        
+# dog = Dog(name="conan", breed="chihuahua")
+# session.add(dog)
+# session.commit()
+        
+# conan = find_by_name(session, 'conan')
+# print("this should be conan", conan)
